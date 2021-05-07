280d6d70f1ad60de60fed5e0a61e70e51df21452
Abraham Zaragoza <41228394+zaragozabraham@users.noreply.github.com>
2021-05-07 18:14:58 -0500

    SplashScreen añadido

--- app/src/main/java/com/example/appgym/MainActivity.java
+++ app/src/main/java/com/example/appgym/MainActivity.java
@@ -25,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.Theme_AppGym_NoActionBar);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
